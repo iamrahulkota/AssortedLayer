d939426e2e2a0b00f4a40f746c27993c9ccba68c
rahul-adya <rahul.kota@adya.ai>
2024-09-17 15:50:40 +0530

    Button functionlity Done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HomeLayout from '../Layout/HomeLayout'
 import About from './About'
 import { useParams } from 'react-router-dom';
@@ -6,13 +6,16 @@ import Card from '../Components/Card/Card';
 
 function Home() {
 
+  const [selectedType, setSelectedType] = useState("All")
 
   const buttons = [
+    { id: 1, name: 'All'},
     { id: 2, name: 'Tools'},
     { id: 3, name: 'Resources' },
     { id: 4, name: 'Technologies' },
     { id: 5, name: 'Upskilling' },
     { id: 6, name: 'Inspiration' },
+    { id: 6, name: 'Typography' },
   ];
 
   console.log(buttons)
@@ -20,36 +23,39 @@ function Home() {
   const cardItems = [
     {
       heading : "Awwwards",
-      description : "Frontend Web Design UI/UX",
+      description : "Awwwards showcase and award websites that demonstrate exceptional design, innovation, and user experience. The platform features a collection of websites that are evaluated by a panel of judges based on criteria such as design, creativity, usability, and content.",
       category : "Inspiration"
     },
     {
-      heading : "Awwwards",
-      description : "Frontend Web Design UI/UX",
+      heading : "Codrops",
+      description : "Keeping web professionals inspired & up-to-date since 2009",
       category : "Inspiration"
     },
     {
-      heading : "Awwwards",
-      description : "Frontend Web Design UI/UX",
+      heading : "Curated.design",
+      description : "Unleash your creativity with out-of-this-world web design inspiration from across the web.",
       category : "Inspiration"
     },
     {
-      heading : "Awwwards",
-      description : "Frontend Web Design UI/UX",
+      heading : "Dark Mode Design",
+      description : "Dark Mode Design is a showcase of beautifully designed and inspiring dark mode websites. Dim the lights, lower your screen brightness, and enjoy.",
       category : "Inspiration"
     },
     {
-      heading : "Awwwards",
-      description : "Frontend Web Design UI/UX",
-      category : "Inspiration"
+      heading : "Atipo Foundry",
+      description : "An independent digital typeface foundry and graphic design studio.",
+      category : "Typography"
     },
 
   ]
 
 
   const handleClick = (BtnName)=>{
+    setSelectedType(BtnName)
     console.log(BtnName)
   } 
+
+  const filteredCards = selectedType === "All" ? cardItems : cardItems.filter(card => card.category === selectedType)
 
 
   return (
@@ -64,7 +70,12 @@ function Home() {
 
         <div className='mx-auto bg-[#121212] py-4 px-10 rounded-lg flex w-full md:w-8/12 mb-24 flex-wrap'>
               {buttons.map((eachButton)=>(
-                <button className='mx-auto font-medium rounded-sm px-6 py-2 hover:bg-white hover:text-black' key={eachButton.id} onClick={()=> handleClick(eachButton.name)}>
+                <button 
+                  className={`mx-auto font-medium rounded-sm px-6 py-2 hover:bg-white hover:text-black 
+                    ${selectedType === eachButton.name ? 'bg-white text-black' : 'text-white'}
+                  `} 
+                  key={eachButton.id} 
+                  onClick={()=> handleClick(eachButton.name)}>
                   {eachButton.name}
                 </button>
               ))}
@@ -72,7 +83,7 @@ function Home() {
         
 
         <div className='flex flex-wrap justify-center'>
-          {cardItems.map((item)=>(
+          {filteredCards.map((item)=>(
             <Card heading={item.heading} description={item.description} category={item.category} />
           ))}
           
