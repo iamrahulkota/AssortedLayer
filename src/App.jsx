ab25759c66544f002ad629c3d27edce739619f2f
rahul-adya <rahulkota0313@gmail.com>
2024-09-18 18:02:38 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ function App() {
 
   return (
     <>
+
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About />} />
       </Routes>
+      
     </>
   )
 }
